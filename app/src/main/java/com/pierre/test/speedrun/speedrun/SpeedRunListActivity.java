204d4cd693d099271e8400b219be9d335fe600be
pithou <pierre.terroitin@gmail.com>
2018-09-14 04:33:54 +0200

    init android project

---
+++ app/src/main/java/com/pierre/test/speedrun/speedrun/SpeedRunListActivity.java
@@ -0,0 +1,13 @@
+package com.pierre.test.speedrun.speedrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeedRunListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speed_run_list);
+    }
+}
